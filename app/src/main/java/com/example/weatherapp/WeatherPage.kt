57485114b219ad8411f67d92cb9f2e4cad1ec12a
khanjaved443 <57727397+khanjaved443@users.noreply.github.com>
2024-06-29 17:13:15 +0530

    UI completed

--- app/src/main/java/com/example/weatherapp/WeatherPage.kt
+++ app/src/main/java/com/example/weatherapp/WeatherPage.kt
@@ -3,10 +3,16 @@ package com.example.weatherapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -20,8 +26,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.weatherapp.api.NetworkResponse
+import com.example.weatherapp.api.WeatherModel
 
 @Composable
 fun WeatherPage(viewModel: WeatherViewModel) {
@@ -29,6 +42,7 @@ fun WeatherPage(viewModel: WeatherViewModel) {
         mutableStateOf("")
     }
     val weatherResult = viewModel.weatherResult.observeAsState()
+    val keyBoardController= LocalSoftwareKeyboardController.current
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -51,7 +65,10 @@ fun WeatherPage(viewModel: WeatherViewModel) {
                 },
                 label = { Text(text = "Enter Location") },
             )
-            IconButton(onClick = { viewModel.getData(city) }) {
+            IconButton(onClick = {
+                viewModel.getData(city)
+                keyBoardController?.hide()
+            }) {
                 Icon(imageVector = Icons.Default.Search, contentDescription = "")
             }
         }
@@ -67,12 +84,92 @@ fun WeatherPage(viewModel: WeatherViewModel) {
             }
 
             is NetworkResponse.Success -> {
-                Text(text = result.data.toString())
+                WeatherDetails(data = result.data)
             }
 
             null -> {
 
             }
         }
+    }
+}
+
+@Composable
+fun WeatherDetails(data: WeatherModel) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.Bottom,
+        ) {
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = "Location Icon",
+                Modifier.size(40.dp)
+            )
+            Text(text = data.location.name, fontSize = 30.sp)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = data.location.country, fontSize = 18.sp, color = Color.Gray)
+
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "${data.current.temp_c} ° C",
+            fontSize = 56.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        AsyncImage(
+            model = "https:${data.current.condition.icon}".replace("64x64", "128x128"),
+            contentDescription = "",
+        )
+        Text(
+            text = data.current.condition.text,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            color = Color.Gray
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Card {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyVal("Humidity", data.current.humidity)
+                    WeatherKeyVal("Wind Speed", data.current.wind_kph+" km/h")
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyVal("UV", data.current.uv)
+                    WeatherKeyVal("Participation", data.current.precip_mm+" mm")
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyVal("Local Time", data.location.localtime.split(" ")[1])
+                    WeatherKeyVal("Local Date", data.location.localtime.split(" ")[0])
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun WeatherKeyVal(key: String, value: String) {
+    Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = value, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Text(text = key, fontWeight = FontWeight.SemiBold, color = Color.Gray)
     }
 }
